3ababbef8576e08af92edd67d1a4c9f3e100cf45
arokasa <annikenkasa@hotmail.no>
2026-09-05 01:13:22 +0200

    fix HTML structure

--- vart-miljoarbeid.php
+++ vart-miljoarbeid.php
@@ -2,11 +2,7 @@
 $page = 'vart-miljoarbeid.php';
 include('header.php'); ?>
 
-<title> | Stordalen Entreprenør AS</title>
-
-</php?>
-
-<title>Vårt miljøarbeid | Stordalen Entreprenør</title>
+<title>Vårt miljøarbeid | Stordalen Entreprenør AS</title>
 <div class="animated-container">
     <div class="animation-1"></div>
 </div>
@@ -19,16 +15,13 @@ include('header.php'); ?>
     </div>
     <div class="contact">
         <div class="container">
-            
             <div class="container-left">
-                <h4></br><b>Stordalen Entreprenør AS | Kvalitet, HMS og Miljø</b></h4><br>
+                <h4><br><b>Stordalen Entreprenør AS | Kvalitet, HMS og Miljø</b></h4><br>
                 <!-- <b>Stordalen Entreprenør AS – Kvalitet, HMS og Miljø</b><br><br> -->
                     Hos Stordalen Entreprenør AS skal vi levere entreprenørtjenester med høy kvalitet, sikker gjennomføring og respekt for miljøet. Vi ønsker å være en samarbeidspartner som kunder kan stole på, og vårt omdømme skal være bygget på fagkunnskap, gjennomføringsevne og gode løsninger.
                     Vi arbeider kontinuerlig med forbedring av våre tjenester, arbeidsmetoder og rutiner for å sikre at vi utvikler oss og leverer enda bedre resultater i morgen enn i dag.
                     <br>
-                
-                    
-                <h4></br><b>Kvalitet</b></h4><br>
+                <h4><br><b>Kvalitet</b></h4><br>
                 Vi skal levere arbeid av høy kvalitet som oppfyller kundens forventninger og bidrar til varige løsninger.
                 
                 <br><b><br>Dette oppnår vi ved å:</b>
@@ -50,7 +43,7 @@ include('header.php'); ?>
                 kompetanseheving og aktivt HMS-arbeid 
                 Sikkerhet skal alltid ha høyeste prioritet.
 
-                <h4></br><b>Miljø</b></h4><br>
+                <h4><br><b>Miljø</b></h4><br>
 
                 Vi ønsker å redusere vår miljøpåvirkning og bidra til en mer bærekraftig anleggsbransje.
                 Dette gjør vi ved å:
@@ -59,15 +52,15 @@ include('header.php'); ?>
                 finne mer effektive og ressursbesparende løsninger 
                 gjenbruke masser der dette er mulig og forsvarlig 
                 ha fokus på håndtering av fremmede og invasive plantearter 
-                redusere avfall gjennom god planlegging og kildesortering 
-
-                <br><br><b>Våre bærekraftsmål</b><br><br>
+                redusere avfall gjennom god planlegging og kildesortering.
+                <br><br>
+                <b>Våre bærekraftsmål</b><br/><br/>
                 Vårt arbeid støtter spesielt:
                 FNs bærekraftsmål 8
                 Vi bidrar gjennom trygge arbeidsplasser, fokus på HMS, kompetanseutvikling og et godt arbeidsmiljø.
                 FNs bærekraftsmål 12
                 Vi bidrar gjennom økt gjenbruk av masser, redusert avfall, høy kildesorteringsgrad og mer effektiv ressursbruk.
-                <br><br><b>Mål for 2026</b><br><br>
+                <br><br><b>Mål for 2026<b><br><br>
                 For perioden 2026 arbeider vi mot følgende mål:
                 øke andelen sortert avfall og gjenbruk av masser på våre prosjekter 
                 redusere drivstofforbruket i virksomheten 
@@ -85,99 +78,26 @@ include('header.php'); ?>
                     <!-- <img src="images/miljøfyrtaarn-logo_2026.png" class="full-icon-logo" alt="miljøfyrtårn"/> -->
 
                 <br><br>
-                    <b>Ønsker du miljørapporten tilsendt på mail?<b><br> Send oss en e-post til:  
+                    <b>Ønsker du miljørapporten tilsendt på mail?</b><br> Send oss en e-post til:
                         <a class="miljo" style="text-transform: lowercase;" href="mailto:post@stordalenas.no">post@stordalenas.no</a>
-                </div>
+
+        
+                    </div>
+                 <div class="container-right">
+                <img class="logo_miljøfyrtaarn_icon" src="img/logo_del_1.png" alt="miljøfyrtårn"/>
+                <img class="logo_miljøfyrtaarn_icon" src="img/logo_del_2.png" alt="miljøfyrtårn"/>
             </div>
-                <!-- Øverst i skjemaet
-
-Nederst i skjemaet -->
-
-                <!-- <h2 class="title">Kontakt oss</h2> -->
-
-                <div class="keyitem last">
-                    <!-- <h3>Stordalen Entreprenør AS</h3>
-                    <div class="item">
-                        <div class="adresse" title="adresse">
-                            <b>Bjørnholtveien 4 <br> 3614 Kongsberg <br><br></b>
-                        </div>
-                        <b>Org. nr:</b> 995 640 791 <br>
-                        <b>Tlf:</b><a href="tel:90276000"> 902 76 000 </a><br>
-                        <b>Epost:</b> <a href="mailto:post@stordalenas.no">post@stordalenas.no</a> <br><br>
-                        <b>Fakturaadresse:</b> <a href="mailto:faktura@stordalenas.no">faktura@stordalenas.no</a>
-                    </div> -->
-
+    
                 </div>
-                <!-- <div class="keyitem">
-                    <h3>Bård Stordalen</h3>
-                    <img src="images/bard.JPG" class="keyPerson" alt="bard_stordalen"/>
-                    <div class="item">
-                        <div class="kontakt">
-                            <b>Daglig leder</b> <br><br>
-                            <b>Tlf:</b><a href="tel:90276000"> 902 76 000 </a><br>
-                            <b>Epost:</b> <a href="mailto:bard@stordalenas.no">bard@stordalenas.no</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="keyitem">
-                    <h3>Knut Stordalen</h3>
-                    <img src="images/knut.JPG" class="keyPerson" alt="knut_stordalen"/>
-                    <div class="item">
-                        <div class="kontakt">
-                            <b>Transportleder</b> <br><br>
-                            <b>Tlf:</b><a href="tel:91679100"> 916 79 100 </a><br>
-                            <b>Epost:</b> <a href="mailto:knut@stordalenas.no">knut@stordalenas.no</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="keyitem">
-                    <h3>Ørjan Stordalen</h3>
-                    <img src="images/orjan.JPG" class="keyPerson" alt="orjan_stordalen"/>
-                    <div class="item">
-                        <div class="kontakt">
-                            <b>Anleggsleder</b> <br><br>
-                            <b>Tlf:</b><a href="tel:91679101"> 916 79 101 </a><br>
-                            <b>Epost:</b> <a href="mailto:orjan@stordalenas.no">orjan@stordalenas.no</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="keyitem">
-                    <h3>Lena Rolfsrud</h3>
-                    <img src="images/lena.JPG" class="keyPerson" alt="lena_rolfsrud"/>
-                    <div class="item">
-                        <div class="kontakt">
-                            <b>Kontormedarbeider</b> <br><br>
-                            <b>Tlf:</b><a href="tel:93442664"> 934 42 664 </a><br>
-                            <b>Epost:</b> <a href="mailto:lena@stordalenas.no">lena@stordalenas.no</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="keyitem">
-                    <h3>Sidsel Stordalen</h3>
-                    <img src="images/sidsel.JPG" class="keyPerson" alt="sidsel_stordalen"/>
-                    <div class="item">
-                        <div class="kontakt">
-                            <b>Kontorleder</b> <br><br>
-                            <b>Tlf:</b><a href="tel:91679106"> 916 79 106 </a><br>
-                            <b>Epost:</b> <a href="mailto:sidsel@stordalenas.no">sidsel@stordalenas.no</a>
-                        </div>
-                    </div>
-                </div> -->
-
-            </div>
-
-
-
+       
+          
         </div>
 
+<!--  -----------------------------------                     ---------------------- -->
+
+
     </div>
-
 </div>
-
-
-<h4>
-</h4>
-
 </div>
 
 
