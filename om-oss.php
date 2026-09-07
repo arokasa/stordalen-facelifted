a710ebb83b07e8764288b6969f441f0dce3666a9
arokasa <annikenkasa@hotmail.no>
2026-09-07 20:09:10 +0200

    add responsiveness to om-oss.php

--- om-oss.php
+++ om-oss.php
@@ -3,71 +3,97 @@ $page = 'om-oss';
 include('header.php'); ?>
 
 <title>Om oss | Stordalen Entreprenør AS</title>
+
 <div class="animated-container">
-    <div class="animation-1">
-    </div>
+    <div class="animation-1"></div>
 </div>
+
 <div class="main" id="main">
+
     <div class="about om-oss">
         <div class="container">
             <h2 class="title">Om oss</h2>
             <div class="keyitem first"></div>
         </div>
     </div>
-<!-- <div class="separate"> -->
-    <div class="img-module-container-img-right">
-        <div class="container upper">
-            <div class="module-img-right" style="background-image: url(img/_8276122.JPG);"></div>
-        </div>
-    </diV>
-    <div class="about om-oss">
-        <div class="img-text-module-container-left">
-            <div class="module-text-left keyitem">
-                <h4>Hvem er vi?</h4><br>
-                Stordalen Entreprenør ble etablert som eget AS i 2010, etter mange år i bransjen der vi har vært engasjert i andre store entreprenørselskaper.
-                Siden da har vi stadig hatt nye og spennende prosjekter på gang. <br><br>
-                Vi er en mellomstor bedrift og består i dag av 19 ansatte. Du finner oss i hjertet av Jondalen, like i utkanten av Kongsberg. <br>
-            </div>
-        </div>
-    </div>
-    <div class="img-module-container-img-left">
-        <div class="container lower">
-<!--            <div class="module-img-left" style="background-image: url(../stordalen-entreprenor/images/P6030050-1.JPG);"></div>-->
-<!--            <div class="module-img-left" style="background-image: url(images/firmabil.JPG);">-->
-            <div class="module-img-left" style="background-image: url(img/P6030050.JPG);">
-<!--                <img src="images/firmabil.JPG" class="module-img-left" alt="gravemaskin"/>-->
-            </div>
-        </div>
-    </diV>
-    <div class="about om-oss">
-        <div class="img-text-module-container-right">
-            <div class="module-text-right keyitem">
-                <h4>Hva vi gjør</h4><br>
-                Vi utfører alt innen grunnarbeider, sprengningsarbeider, VA-arbeider, utomhus/grønt-arbeider, veivedlikehold, vinterbrøyting og massetransport.<br><br>
 
-                Vi er sentralt godkjent for flere fagområder i tiltaksklasse 2, og har gode referanser fra tidligere større og mindre prosjekter -
-                både for stat, kommune og private byggherer. <br>
-            </div>
+
+    <!-- Hvem er vi? -->
+    <section class="omoss-section image-right">
+
+        <div class="omoss-text">
+            <h4>Hvem er vi?</h4>
+            <p>
+                Stordalen Entreprenør ble etablert som eget AS i 2010, etter mange år i bransjen
+                der vi har vært engasjert i andre store entreprenørselskaper.
+            </p>
+
+            <p>
+                Siden da har vi stadig hatt nye og spennende prosjekter på gang.
+            </p>
+
+            <p>
+                Vi er en mellomstor bedrift og består i dag av 19 ansatte.
+                Du finner oss i hjertet av Jondalen, like i utkanten av Kongsberg.
+            </p>
         </div>
-    </div>
-    <div class="img-module-container-img-right">
-        <div class="container upper">
-            <div class="module-img-right" style="background-image: url(img/P6031332-3.JPG);"></div>
+
+        <div class="omoss-image"
+             style="background-image: url('img/_8276122.JPG');">
         </div>
-    </diV>
-    <div class="about om-oss">
-        <div class="img-text-module-container-left">
-            <div class="module-text-left keyitem">
-                <h4>Maskinpark</h4><br>
-                Vår maskinpark består av blant annet beltegravere fra 8-23 tonn, hjulgravere med henger, lastebiler for massetransport samt henger for maskintransport.
-                I tillegg til dette har vi utstyr for landmåling, og flere av våre ansatte har ADK-bevis.
-            </div>
+
+    </section>
+
+
+    <!-- Hva vi gjør -->
+    <section class="omoss-section image-left">
+
+        <div class="omoss-text">
+            <h4>Hva vi gjør</h4>
+
+            <p>
+                Vi utfører alt innen grunnarbeider, sprengningsarbeider, VA-arbeider,
+                utomhus/grønt-arbeider, veivedlikehold, vinterbrøyting og massetransport.
+            </p>
+
+            <p>
+                Vi er sentralt godkjent for flere fagområder i tiltaksklasse 2,
+                og har gode referanser fra tidligere større og mindre prosjekter -
+                både for stat, kommune og private byggherrer.
+            </p>
         </div>
-    </div>
-    <!-- <div class="separate"> -->
+
+        <div class="omoss-image"
+             style="background-image: url('img/P6030050.JPG');">
+        </div>
+
+    </section>
+
+
+    <!-- Maskinpark -->
+    <section class="omoss-section image-right">
+
+        <div class="omoss-text">
+            <h4>Maskinpark</h4>
+
+            <p>
+                Vår maskinpark består av blant annet beltegravere fra 8-23 tonn,
+                hjulgravere med henger, lastebiler for massetransport samt henger
+                for maskintransport.
+            </p>
+
+            <p>
+                I tillegg til dette har vi utstyr for landmåling,
+                og flere av våre ansatte har ADK-bevis.
+            </p>
+        </div>
+
+        <div class="omoss-image"
+             style="background-image: url('img/P6031332-3.JPG');">
+        </div>
+
+    </section>
 
 </div>
 
-
 <?php include('footer.php'); ?>
-<?php	?>
