04a2048f4ab0bec84c0cb4671e9fa42bc937dede
arokasa <annikenkasa@hotmail.no>
2026-09-21 18:31:43 +0200

    cleanup karriere.php

--- karriere.php
+++ karriere.php
@@ -3,54 +3,84 @@ $page = 'karriere';
 include('header.php'); ?>
 
 <title>Karriere | Stordalen Entreprenør AS</title>
+
 <div class="animated-container">
-    <div class="animation-1">
-    </div>
+    <div class="animation-1"></div>
 </div>
+
 <div class="main" id="main">
-    <div class="about om-oss">
-        <div class="container">
-            <h2 class="title">Karriere</h2>
-            <div class="keyitem first"></div>
-        </div>
-    </div>
-    <div class="img-module-container-img-right">
-        <div class="container upper">
-            <div class="module-img-right" style="background-image: url(img/P6031081.JPG);"></div>
-        </div>
-    </diV>
-    <div class="about om-oss">
-        <div class="img-text-module-container-left">
-            <div class="module-text-left keyitem">
-                <h4>Kunne du tenkt deg å jobbe hos oss?</h4><br>
-                Om du har lyst til å jobbe hos oss, kan du følge med på <a href="https://www.facebook.com/Stordalen-Entrepren%C3%B8r-As-365350660208729/" target="_blank" rel="noopener noreferrer" class="normal-link"><b>Facebook-siden vår</b></a>,
-                eller ta <a href="kontakt.php" class="normal-link"><b>ta kontakt med oss</b></a>.
-            </div>
-        </div>
-    </div>
-    <div class="img-module-container-img-left">
-        <div class="container lower">
-            <div class="module-img-left" style="background-image: url(img/P6031365-0.JPG);">
-            </div>
-        </div>
-    </diV>
-    <div class="about om-oss">
-        <div class="img-text-module-container-right">
-            <div class="module-text-right keyitem">
-                <h4>For lærlinger</h4><br>
-                Stordalen Entreprenør AS er godkjent lærebedrift for <b> anleggsmaskinførerfaget, vei- og anleggsfaget og yrkessjåførfaget</b>.
-                Mer om våre godkjenninger og opplæringstilbud kan du finne på <a href="https://utdanning.no/bedrift/995640791" target="_blank" rel="noopener noreferrer" class="normal-link"><b>utdanning.no</b></a> og
-                <a href="https://www.vilbli.no/nb/nb/viken/stordalen-entreprenor-as/adr/963317" target="_blank" rel="noopener noreferrer" class="normal-link"><b>vilbli.no</b></a>.
-                <br><br>
-                Som lærling i Stordalen Entreprenør vil du bli godt ivaretatt, og vi har i dag flere ansatte som startet sin karriere med lærlingtiden sin hos oss.
-                <br><br>
-                Send oss gjerne en e-post med din søknad som vedlegg til <a href="mailto:post@stordalenas.no" class="normal-link"><b>post@stordalenas.no</b></a>.
-                Vi gleder oss til å høre fra deg!
-            </div>
-        </div>
+
+```
+<div class="about om-oss">
+    <div class="container">
+        <h2 class="title">Karriere</h2>
+        <div class="keyitem first"></div>
     </div>
 </div>
 
+<!-- Kunne du tenkt deg å jobbe hos oss? -->
+<section class="omoss-section image-right">
+    <div class="omoss-text">
+        <h4>Kunne du tenkt deg å jobbe hos oss?</h4>
+
+        <p>
+            Om du har lyst til å jobbe hos oss, kan du følge med på
+            <a href="https://www.facebook.com/Stordalen-Entrepren%C3%B8r-As-365350660208729/"
+               target="_blank"
+               rel="noopener noreferrer"
+               class="normal-link"><b>Facebook-siden vår</b></a>,
+            eller <a href="kontakt.php" class="normal-link"><b>ta kontakt med oss</b></a>.
+        </p>
+    </div>
+
+    <div class="omoss-image"
+         style="background-image: url('img/P6031081.JPG');">
+    </div>
+</section>
+
+<!-- For lærlinger -->
+<section class="omoss-section image-left">
+    <div class="omoss-text">
+        <h4>For lærlinger</h4>
+
+        <p>
+            Stordalen Entreprenør AS er godkjent lærebedrift for
+            <b>anleggsmaskinførerfaget, vei- og anleggsfaget og yrkessjåførfaget</b>.
+        </p>
+
+        <p>
+            Mer om våre godkjenninger og opplæringstilbud kan du finne på
+            <a href="https://utdanning.no/bedrift/995640791"
+               target="_blank"
+               rel="noopener noreferrer"
+               class="normal-link"><b>utdanning.no</b></a>
+            og
+            <a href="https://www.vilbli.no/nb/nb/viken/stordalen-entreprenor-as/adr/963317"
+               target="_blank"
+               rel="noopener noreferrer"
+               class="normal-link"><b>vilbli.no</b></a>.
+        </p>
+
+        <p>
+            Som lærling i Stordalen Entreprenør vil du bli godt ivaretatt,
+            og vi har i dag flere ansatte som startet sin karriere med
+            lærlingtiden sin hos oss.
+        </p>
+
+        <p>
+            Send oss gjerne en e-post med din søknad som vedlegg til
+            <a href="mailto:post@stordalenas.no" class="normal-link"><b>post@stordalenas.no</b></a>.
+            Vi gleder oss til å høre fra deg!
+        </p>
+    </div>
+
+    <div class="omoss-image"
+         style="background-image: url('img/P6031365-0.JPG');">
+    </div>
+</section>
+
+</div>
 
 <?php include('footer.php'); ?>
-<?php	?>
+
+<?php ?>
